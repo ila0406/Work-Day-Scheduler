97dbad64df8d4f476dca2063c8117e5b8b8bc00d
Psyche46 <ila0406@gmail.com>
2022-02-01 08:35:17 -0700

    Updated JS with variables and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
+// Variables for time
 var todayDate = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(todayDate);
+var now = new Date().getHours();
+  
+  // Based on time var 'now' store past/present/future in local storage
+  function storePastPresent() {
+    for (let i = 8; i < 19; i++) {
+      if (now > i) {
+        $(`#projectTime${i}`).addClass("past");
+      } else if (now == i) {
+        $(`#projectTime${i}`).addClass("present");
+      } else {
+        $(`#projectTime${i}`).addClass("future");
+      }
+      //Store in Local Storage
+      $(`#projectTime${i}`).val(localStorage.getItem(i));
+    }
+  }
+  
+  // Save icons within the App
+  function button(arg) {
+    localStorage.setItem(arg, $(`#projectTime${arg}`).val());
+  }
+  
+  // A call to the function
+  storePastPresent();
